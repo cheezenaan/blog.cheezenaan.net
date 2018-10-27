b15417313f40ca9e04ea0458ac05af5a55a8bdf9
cheezenaan <cheezenaan@gmail.com>
2018-10-27 13:46:27 +0900

    enhance(gatsby): Create static pages from markdown

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,5 +3,43 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+const path = require('path');
 
-// You can delete this file if you're not using it
+const createPages = async ({ actions, graphql }) => {
+  const { createPage } = actions;
+  const blogPostTemplate = path.resolve(`src/templates/blog-post.tsx`);
+
+  const result = await graphql(`
+    {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            html
+            frontmatter {
+              date
+              path
+              title
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  if (result.errors) {
+    return Promise.reject(result.errors);
+  }
+
+  return result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.frontmatter.path,
+      component: blogPostTemplate,
+      context: {},
+    });
+  });
+};
+
+exports.createPages = createPages;
