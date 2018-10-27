fb1d13b8a7deccb6995b7b14306261bdafbe55fa
cheezenaan <cheezenaan@gmail.com>
2018-10-27 11:53:11 +0900

    enhance(gatsby): Introduce-gatsby-plugin-catch-links

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
     title: 'Gatsby Default Starter',
   },
   plugins: [
+    'gatsby-plugin-catch-links',
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-manifest`,
