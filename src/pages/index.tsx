9694638b1d4c0b7671fb6e1b7686ee33a1ef0067
cheezenaan <cheezenaan@gmail.com>
2018-10-27 14:45:36 +0900

    enhance(page): Render blog page list in IndexPage component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,66 @@
-import { Link } from 'gatsby';
+import { graphql, Link } from 'gatsby';
 import * as React from 'react';
 
 import { Layout } from '../components/layout';
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-);
+interface Post {
+  node: {
+    excerpt: string;
+    id: number;
+    frontmatter: {
+      title: string;
+      date: string;
+      path: string;
+    };
+  };
+}
+
+interface Props {
+  data: {
+    allMarkdownRemark: {
+      edges: Post[];
+    };
+  };
+}
+
+const IndexPage: React.SFC<Props> = ({ data }) => {
+  const { edges: posts } = data.allMarkdownRemark;
+
+  return (
+    <Layout>
+      <div className="blog-posts">
+        {posts
+          .filter(({ node: post }) => post.frontmatter.title.length > 0)
+          .map(({ node: post }) => (
+            <div className="blog-post-preview" key={post.id}>
+              <h1>
+                <Link to={post.frontmatter.path}>{post.frontmatter.title}</Link>
+              </h1>
+              <h2>{post.frontmatter.date}</h2>
+              <p>{post.excerpt}</p>
+            </div>
+          ))}
+      </div>
+    </Layout>
+  );
+};
 
 export default IndexPage;
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          excerpt(pruneLength: 280)
+          id
+          frontmatter {
+            title
+            date(formatString: "MMMM DD, YYYY")
+            path
+          }
+        }
+      }
+    }
+  }
+`;
