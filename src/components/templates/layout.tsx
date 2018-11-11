f9a0efaae57581880356da8cfb4d4381abc4b02c
cheezenaan <cheezenaan@gmail.com>
2018-11-11 12:21:23 +0900

    feat(organism): Add Footer component

--- src/components/templates/layout.tsx
+++ src/components/templates/layout.tsx
@@ -2,11 +2,13 @@ import './layout.sass';
 
 import * as React from 'react';
 
+import { Footer } from '../organisms/footer';
 import { WrappedMetatag as Metatag } from '../organisms/metatag';
 
 export const Layout: React.SFC = ({ children }) => (
   <>
     <Metatag />
     {children}
+    <Footer />
   </>
 );
