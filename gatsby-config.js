9692561fd9280400f5a27943d23862ae17b10987
cheezenaan <cheezenaan@gmail.com>
2018-11-11 18:16:16 +0900

    enhance(manifest): Fix manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,13 +27,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'blog.cheezenaan.net',
+        name: 'blog.cheezenaan.net',
+        short_name: 'blog',
         start_url: '/',
         background_color: `#fafafa`,
         theme_color: `#fafafa`,
         display: 'minimal-ui',
-        icon: 'src/images/curry-and-rice.png', // This path is relative to the root of the site.
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-offline',
