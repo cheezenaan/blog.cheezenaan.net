8b1c27a95b82ea0157b9ed871d13484b6900a28a
cheezenaan <cheezenaan@gmail.com>
2018-11-13 09:48:23 +0900

    feat(layout): Initialize FontAwesome icons

--- src/components/templates/layout.tsx
+++ src/components/templates/layout.tsx
@@ -1,10 +1,15 @@
 import './layout.sass';
 import 'prismjs/themes/prism.css';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 import * as React from 'react';
 
 import { Footer } from '../organisms/footer';
 import { Metatag } from '../organisms/metatag';
+
+// Use FontAwesome icons in React components
+library.add(fab);
 
 export const Layout: React.SFC = ({ children }) => (
   <>
