4f89bf44a7e66e40de8b1f68c1d00646cd321fdf
cheezenaan <cheezenaan@gmail.com>
2018-11-17 20:07:40 +0900

    refactor(organism): Remove Metatag component
    because it's non-reusable so aggregate into Layout component

--- src/components/templates/layout.tsx
+++ src/components/templates/layout.tsx
@@ -4,9 +4,9 @@ import 'prismjs/themes/prism.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import * as React from 'react';
+import { Helmet } from 'react-helmet';
 
 import { Footer } from '../organisms/footer';
-import { Metatag } from '../organisms/metatag';
 import { NavigationHeader } from '../organisms/navigation-header';
 import { SiteMetadataProvider } from '../providers/site-metadata';
 
@@ -23,7 +23,12 @@ export const Layout: React.SFC<Props> = ({ pageTitle, titleTag, children }) => (
   <SiteMetadataProvider
     render={({ siteTitle }) => (
       <>
-        <Metatag siteTitle={siteTitle} pageTitle={pageTitle} />
+        <Helmet
+          title={pageTitle ? `${pageTitle} - ${siteTitle}` : siteTitle}
+          meta={[{ name: 'description', content: 'Private blog' }]}
+        >
+          <html lang="ja" />
+        </Helmet>
         <NavigationHeader siteTitle={siteTitle} titleTag={titleTag} />
         {children}
         <Footer />
